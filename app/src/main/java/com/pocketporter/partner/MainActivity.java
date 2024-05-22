082917e4ad2e08e316223223003c4f09e012a240
sangharsh-codexxa <sangharsh@codexxa.in>
2024-05-22 13:20:05 +0530

    First Updated Code

---
+++ app/src/main/java/com/pocketporter/partner/MainActivity.java
@@ -0,0 +1,15 @@
+package com.codexxatransco.partner;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.codexxatransco.partner.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
